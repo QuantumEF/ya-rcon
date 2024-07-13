bbb06cc1b9e3621155dfb2afe0c6a38ad72b065f
Erik Fong <quantumerikfong@gmail.com>
2024-07-13 13:01:52 -0400

    Initial ID struct

---
+++ src/packet/packet_id.rs
@@ -0,0 +1,23 @@
+/// This struct is about explicitly stating how the ID is handled see `from_wrapping` for more info
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd, Eq, Ord)]
+pub struct ID(i32);
+
+impl ID {
+    /// Valid IDs are positive integers, but it can take on negative values when the server wants to indicate an error.
+    /// This takes u32 to ensure the value is possitive and wraps it simple by using `id as i32`
+    pub fn from_wrapping(id: u32) -> ID {
+        ID(id as i32)
+    }
+}
+
+impl From<ID> for i32 {
+    fn from(value: ID) -> Self {
+        value.0
+    }
+}
+
+impl From<i32> for ID {
+    fn from(value: i32) -> Self {
+        ID(value)
+    }
+}
