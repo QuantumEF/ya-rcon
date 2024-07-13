cc4f4292df6c88b3d4b83abcbc280a9f56e91bff
Erik Fong <quantumerikfong@gmail.com>
2024-07-13 11:32:57 -0400

    reexport and simple rcon client

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,17 @@
+use std::net::TcpStream;
+
+pub use client::RCONClient;
+
 pub mod client;
 pub mod packet;
+
+pub fn simple_tcp_client(
+    addr: impl std::net::ToSocketAddrs,
+    password: String,
+) -> std::result::Result<
+    client::RCONClient<std::net::TcpStream, std::ops::RangeFrom<u32>>,
+    std::io::Error,
+> {
+    let stream = TcpStream::connect(addr)?;
+    Ok(RCONClient::new(stream, 0.., password)?)
+}
